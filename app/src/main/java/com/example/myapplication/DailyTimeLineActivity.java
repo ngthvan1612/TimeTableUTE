8dea89a5a208a3a246cbc39349c3b79b6d2bec41
ngthvan1612@gmail.com <ngthvan1612@gmail.com>
2023-03-10 01:08:31 +0700

    create daily time activity

---
+++ app/src/main/java/com/example/myapplication/DailyTimeLineActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DailyTimeLineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_time_line);
+    }
+}
